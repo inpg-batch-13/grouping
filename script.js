4e1c484eb025d988502ef9052c48a9fa82b72579
danielevanc <danielevanc2@gmail.com>
2019-06-24 21:00:07 +0700

    grouping

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function grouping (scores) {
-  // Your code here
+  var hasil = {pass: [],fail: []}
+  for (i = 0 ; i < scores.length ; i++){
+    if(scores[i] > 75){
+      hasil.pass.push(scores[i])
+    } hasil.fail.push(scores[i])
+  }
+  return hasil
 }
 
 console.log(grouping([80, 45, 90, 65, 74, 100, 85, 30]));
